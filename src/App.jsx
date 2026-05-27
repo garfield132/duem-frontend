e1e0357035043503e6325955548a800ea27eae6d
Suphakrit <kiritosinon89@gmail.com>
2026-05-27 12:41:17 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -991,7 +991,7 @@ export default function App() {
         <span
           style={{
             color: 'rgba(255,255,255,0.58)',
-            fontSize: 1,
+            fontSize: 10,
             lineHeight: 1.6,
           }}
         >
